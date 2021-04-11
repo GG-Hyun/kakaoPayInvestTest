5a8029c3c77ed72b0d540cd3d3a5076ca07d1b96
admin <admin@DESKTOP-DEBRTHC>
2021-04-11 23:31:44 +0900

    Create Project

---
+++ kakaoPayTest/src/kakaoPayTest/codeTest1.java
@@ -0,0 +1,16 @@
+package kakaoPayTest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class codeTest1 {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dddd
+	}
+
+}
